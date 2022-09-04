fe45c0bb29db2eb5761dc6f403864c9819a1d516
Karthiga <karthigaboopathi2000@gmail.com>
2022-09-04 14:40:32 +0530

    added

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace project_practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string data = File.ReadAllText("D:\\Practice\\practiceproject\\studentdata.txt");
+            Console.WriteLine(data);
+            Console.ReadLine();
+        }
+    }
+}
